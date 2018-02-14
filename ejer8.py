ec641106906f475f5919a98cdaf770edeabe4a9d
Nacho Gomez <nacho.gomez5293@gmail.com>
2018-02-14 19:35:46 +0100

    Ejercicios de Funciones

---
+++ ejer8.py
@@ -0,0 +1,9 @@
+import misfunciones
+
+
+misfunciones.imprimir()
+
+print misfunciones.funcion()
+print misfunciones.par_impar(30)
+print misfunciones.par_impar1(30)
+print misfunciones.alta_cliente("555555F","Nacho","Gomez","Garcia")
---
+++ misfunciones.py
@@ -0,0 +1,21 @@
+def imprimir():
+	print'hola'
+	
+def funcion():
+	return "Hola Mundo"
+	
+def par_impar(numero):
+	if numero%2==0:
+		return "par"
+	else:
+		return "impar"
+
+def par_impar1(numero):
+	return "par" if(numero%2==0) else "impar"
+	
+def alta_cliente(dni,nombre,*apellidos):
+	print dni
+	print nombre
+	print "Tienes "+str(len(apellidos))+" apellidos"
+	for x in apellidos:
+		print x
